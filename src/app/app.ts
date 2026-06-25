72e07d1dceb24bb19862922116c914b72bbf1afa
Natan20 <natanaelsantossi@hotmail.com>
2026-06-25 12:56:08 -0300

    Iniciando a página de login

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet], 
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
+
+
 export class App {
-  protected readonly title = signal('desafio-angula');
+  protected readonly title = ('desafio-angula');
 }
